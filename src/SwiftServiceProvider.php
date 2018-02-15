f9c3d7b344be6cafe1660a0be159bb544e671c90
Martin Zurowietz <martin@zurowietz.de>
2018-02-15 13:20:47 +0100

    Implement caching of authentication token for much faster requests

--- src/SwiftServiceProvider.php
+++ src/SwiftServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace Mzur\Filesystem;
 
+use Cache;
 use Storage;
+use DateTime;
 use OpenStack\OpenStack;
 use League\Flysystem\Filesystem;
 use Illuminate\Support\ServiceProvider;
@@ -18,7 +20,7 @@ class SwiftServiceProvider extends ServiceProvider
     public function boot()
     {
         Storage::extend('swift', function($app, $config) {
-            $openstack = new OpenStack([
+            $params = [
                 'authUrl' => $config['authUrl'],
                 'region' => $config['region'],
                 'user' => [
@@ -27,8 +29,25 @@ class SwiftServiceProvider extends ServiceProvider
                     'domain' => ['name' => $config['domain']],
                 ],
                 'scope' => ['project' => ['id' => $config['projectId']]],
-            ]);
+            ];
 
+            $cachedTokenKey = "openstack-swift-token-{$config['user']}-{$config['domain']}";
+
+            // Cache the authentication token to significantly speed up requests.
+            // See: http://php-openstack-sdk.readthedocs.io/en/identity-v2/services/identity/v3/tokens.html#cache-authentication-token
+            if (Cache::has($cachedTokenKey)) {
+                $params['cachedToken'] = Cache::get($cachedTokenKey);
+            } else {
+                $openstack = new OpenStack(['authUrl' => $config['authUrl']]);
+                $token = $openstack->identityV3()->generateToken($params);
+                $params['cachedToken'] = $token->export();
+                // Convert DateTimeImmutable to DateTime because Cache::put expects
+                // the latter to determine the expiration.
+                $expires = new DateTime($token->expires->format('c'));
+                Cache::put($cachedTokenKey, $params['cachedToken'], $expires);
+            }
+
+            $openstack = new OpenStack($params);
             $container = $openstack->objectStoreV1()->getContainer($config['container']);
 
             return new Filesystem(new SwiftAdapter($container));
