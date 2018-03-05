1958806174453543bd6a12e0a20f72fd0da806ad
Martin Zurowietz <martin@zurowietz.de>
2018-03-05 12:44:36 +0100

    Implement wrapper for SwiftAdapter to automatically refresh tokens
    Tokens need to be refreshed in long running scripts like Laravel
    queue workers.

--- src/SwiftServiceProvider.php
+++ src/SwiftServiceProvider.php
@@ -2,14 +2,10 @@
 
 namespace Mzur\Filesystem;
 
-use Cache;
 use Storage;
-use DateTime;
-use OpenStack\OpenStack;
 use League\Flysystem\Filesystem;
 use Illuminate\Support\ServiceProvider;
 use League\Flysystem\Config as FlyConfig;
-use Nimbusoft\Flysystem\OpenStack\SwiftAdapter;
 
 class SwiftServiceProvider extends ServiceProvider
 {
@@ -40,10 +36,9 @@ class SwiftServiceProvider extends ServiceProvider
                 $params['scope'] = ['project' => ['id' => $config['projectId']]];
             }
 
-            $openstack = $this->getOpenStack($params);
-            $container = $openstack->objectStoreV1()->getContainer($config['container']);
+            $wrapper = new SwiftAdapterWrapper($params, $config['container']);
 
-            return new Filesystem(new SwiftAdapter($container), new FlyConfig([
+            return new Filesystem($wrapper, new FlyConfig([
                 'disable_asserts' => array_get($config, 'disableAsserts', false),
             ]));
         });
@@ -57,33 +52,5 @@ class SwiftServiceProvider extends ServiceProvider
     public function register()
     {
         //
-    }
-
-    /**
-     * Get the OpenStack instance.
-     *
-     * @param array $params
-     *
-     * @return OpenStack
-     */
-    protected function getOpenStack(array $params)
-    {
-        $cachedTokenKey = "openstack-swift-token-{$params['user']['name']}-{$params['user']['domain']['name']}";
-
-        // Cache the authentication token to significantly speed up requests.
-        // See: http://php-openstack-sdk.readthedocs.io/en/identity-v2/services/identity/v3/tokens.html#cache-authentication-token
-        if (Cache::has($cachedTokenKey)) {
-            $params['cachedToken'] = Cache::get($cachedTokenKey);
-        } else {
-            $openstack = new OpenStack(['authUrl' => $params['authUrl']]);
-            $token = $openstack->identityV3()->generateToken($params);
-            $params['cachedToken'] = $token->export();
-            // Convert DateTimeImmutable to DateTime because Cache::put expects
-            // the latter to determine the expiration.
-            $expires = new DateTime($token->expires->format('c'));
-            Cache::put($cachedTokenKey, $params['cachedToken'], $expires);
-        }
-
-        return new OpenStack($params);
     }
 }
